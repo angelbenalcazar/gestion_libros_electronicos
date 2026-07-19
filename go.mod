e207040dfe55c891a878401d09d770c0e52113c9
angelbenalcazar <angelbenalcazar178@gmail.com>
2026-07-19 12:35:07 -0500

    Initial commit: estructura del sistema de gestión de libros electrónicos

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/angelbenalcazar/gestion_libros_electronicos
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Sistema de Gestión de Libros Electrónicos")
+
+}
